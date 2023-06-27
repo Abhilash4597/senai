837fe1fc5a63e81baf3071b30d991c6a2e73f3ac
Abhilash4597 <abhilash4597@gmail.com>
2023-06-28 02:24:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,16 @@ function App() {
     <>
       <div className="container">
         <div className="main_container">
-          <div className="main_left_side"></div>
-          <div className="main_right_side"></div>
+          <div className="main_left_side">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis perferendis sed doloremque ipsam impedit quis repudiandae iure vitae consequatur. Maiores quidem quod explicabo consequuntur id obcaecati quae, eveniet totam facilis.
+          </div>
+          <div className="main_right_side">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum voluptatem veritatis accusantium laboriosam ea praesentium quis incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit tempora, expedita dicta doloremque perferendis dolore! Delectus commodi sed possimus doloribus corporis reiciendis adipisci iure obcaecati!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum voluptatem veritatis accusantium laboriosam ea praesentium quis incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit tempora, expedita dicta doloremque perferendis dolore! Delectus commodi sed possimus doloribus corporis reiciendis adipisci iure obcaecati!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum voluptatem veritatis accusantium laboriosam ea praesentium quis incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit tempora, expedita dicta doloremque perferendis dolore! Delectus commodi sed possimus doloribus corporis reiciendis adipisci iure obcaecati!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum voluptatem veritatis accusantium laboriosam ea praesentium quis incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit tempora, expedita dicta doloremque perferendis dolore! Delectus commodi sed possimus doloribus corporis reiciendis adipisci iure obcaecati!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum voluptatem veritatis accusantium laboriosam ea praesentium quis incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit tempora, expedita dicta doloremque perferendis dolore! Delectus commodi sed possimus doloribus corporis reiciendis adipisci iure obcaecati!
+          </div>
         </div>
       </div>
     </>
