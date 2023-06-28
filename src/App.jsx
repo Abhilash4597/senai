b97949196f9b684e95a9a02b67e1e64c4087f638
Abhilash4597 <abhilash4597@gmail.com>
2023-06-28 16:28:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,61 +3,29 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="container">
+      <div className="container flex">
         <div className="main_container">
           <div className="main_left_side">
-            <div className="main_left-left">
-              <span className="material-symbols-outlined">add</span>
+            <div className="main_left-header flex">
+              <span className="material-symbols-outlined color-size">
+                add_box
+              </span>
             </div>
-            <div className="main_left-middle">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
-              fugit unde accusamus tempore consequatur debitis rerum libero vel
-              ullam veritatis, incidunt ipsum quod nemo repellendus est dolores
-              pariatur mollitia reiciendis.
+            <div className="main_left-header flex">
+              <span className="material-symbols-outlined color-size align">
+                steppers
+              </span>
             </div>
-            <div className="main_left-right">
-              <span className="material-symbols-outlined">switches</span>
+            <div className="main_left-header flex">
+              <span className="temp">&#8451;</span>
+              <span className="material-symbols-outlined color-size large align">
+                switches
+              </span>
+              <span className="temp">&#8457;</span>
             </div>
           </div>
 
-          <div className="main_right_side">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae
-            possimus dicta, suscipit tenetur et nihil aperiam iusto illum
-            voluptatem veritatis accusantium laboriosam ea praesentium quis
-            incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit
-            tempora, expedita dicta doloremque perferendis dolore! Delectus
-            commodi sed possimus doloribus corporis reiciendis adipisci iure
-            obcaecati! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum
-            voluptatem veritatis accusantium laboriosam ea praesentium quis
-            incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit
-            tempora, expedita dicta doloremque perferendis dolore! Delectus
-            commodi sed possimus doloribus corporis reiciendis adipisci iure
-            obcaecati! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum
-            voluptatem veritatis accusantium laboriosam ea praesentium quis
-            incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit
-            tempora, expedita dicta doloremque perferendis dolore! Delectus
-            commodi sed possimus doloribus corporis reiciendis adipisci iure
-            obcaecati! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum
-            voluptatem veritatis accusantium laboriosam ea praesentium quis
-            incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit
-            tempora, expedita dicta doloremque perferendis dolore! Delectus
-            commodi sed possimus doloribus corporis reiciendis adipisci iure
-            obcaecati! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Quae possimus dicta, suscipit tenetur et nihil aperiam iusto illum
-            voluptatem veritatis accusantium laboriosam ea praesentium quis
-            incidunt itaque, qui ut nam. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Explicabo sunt quia qui ducimus reprehenderit
-            tempora, expedita dicta doloremque perferendis dolore! Delectus
-            commodi sed possimus doloribus corporis reiciendis adipisci iure
-            obcaecati!
-          </div>
+          <div className="main_right_side"></div>
         </div>
       </div>
     </>
